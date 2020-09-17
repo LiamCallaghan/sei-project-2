a8f2ab4f765269fe6476cc87a9bfc306a2faf3c8
Liam Callaghan <ljcallaghan64@gmail.com>
2020-09-17 12:22:18 +0100

    Home page layout

--- src/component/common/Home.js
+++ src/component/common/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-class Home extends React.Component {
-
-  render() {
-return 
-  }
+const Home = () => {
+  return (
+    <section className="hero is-fullheight-with-navbar">
+      <div className="hero-body">
+        <img src='https://files.slack.com/files-pri/T0351JZQ0-F01BKKQK8N4/image.png' />
+        <div className="container">
+          <h1 className="title is-1 has-text-centered has-text-black">
+             Quiz
+          </h1>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Home
