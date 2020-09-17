cce31e20df7034d3e8c20287714c773b1e9b7580
Liam Callaghan <ljcallaghan64@gmail.com>
2020-09-17 20:05:43 +0100

    questions

--- src/component/game/Quiz.js
+++ src/component/game/Quiz.js
@@ -4,44 +4,56 @@ import { getSingleCategory } from '../../lib/api'
 
 class Quiz extends React.Component {
   state = {
-    questions: null
+    categories: null,
+    questions: [],
+    answers: [],
+    incorrect_answers: []
   }
 
   async componentDidMount() {
     const catagoryId = this.props.match.params.id
     const response = await getSingleCategory(catagoryId)
     this.setState({
-      questions: response.data
+      categories: response.data
     })
-    // console.log(response.data)
-    { this.questionLoop() }
+    this.questionLoop()
+    this.answerLoop()
+    this.incorrectLoop()
 
   }
 
   questionLoop = () => {
-    const questions = this.state.questions.results.map(result => (result.question))
-    return questions   
+    const questions = this.state.categories.results.map(result => (result.question))
+        this.setState({
+      questions
+    }) 
   }
   answerLoop = () => {
-    const answer = this.state.questions.results.map(result => (result.correct_answer))
-    return answer  
+    const answers = this.state.categories.results.map(result => (result.correct_answer))
+        this.setState({
+      answers
+    })  
   }
   incorrectLoop = () => {
-    const incorrectAnswers = this.state.questions.results.map(result => (result.incorrect_answers))
-    const incorrectAnswer = incorrectAnswers.map(result => (result))
-    return incorrectAnswer  
-
+    const incorrect_answers = this.state.categories.results.map(result => (result.incorrect_answers))
+    const incorrect_answer = incorrect_answers.map(result => (result))
+        this.setState({
+      incorrect_answers: incorrect_answer
+    })  
   }
+
+
 
 
   render() {
+    const { questions, answers, incorrect_answers } = this.state
     if ( !this.state.questions ) return null
     return (
       <section className="section">
         <div className="container">
-          {this.questionLoop().map(question => (<h1 key={[question]}>{question}</h1>))}
-          {this.answerLoop().map(answer => (<h1 key={[answer]}>{answer}</h1>))}
-          {this.incorrectLoop().map(wrong => (<h1 key={[wrong]}>{wrong}</h1>))}
+          <div>{ questions[0] }</div>
+          <div>{ answers[0] }</div>
+          <div>{ incorrect_answers[0] }</div>
         </div>
 
       </section>
