9832c2c74e2684420dffb7006b7d88fbb5e28387
Anouska Ralph <anouskaoleary@gmail.com>
2020-09-17 13:09:30 +0100

    added categories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './component/common/Home'
+import Categories from './component/game/Categories'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route path="/game" component={Categories}/>
       </Switch>
     </BrowserRouter>
   )
