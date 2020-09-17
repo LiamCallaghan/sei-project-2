7e2686c0d67b73d857c6b183ca1114780fccd95a
Liam Callaghan <ljcallaghan64@gmail.com>
2020-09-17 14:14:06 +0100

    catagories urls

--- src/component/game/Categories.js
+++ src/component/game/Categories.js
@@ -6,5 +6,15 @@ const Categories = () => {
   ) 
 }
 
+// https://opentdb.com/api.php?amount=10&category=10&type=multiple
+// https://opentdb.com/api.php?amount=10&category=11&type=multiple
+// https://opentdb.com/api.php?amount=10&category=12&type=multiple
+//https://opentdb.com/api.php?amount=10&category=15&type=multiple
+// https://opentdb.com/api.php?amount=10&category=17&type=multiple
+// https://opentdb.com/api.php?amount=10&category=18&type=multiple
+// https://opentdb.com/api.php?amount=10&category=23&type=multiple
+// https://opentdb.com/api.php?amount=10&category=25&type=multiple
+// https://opentdb.com/api.php?amount=10&category=27&type=multiple
+// https://opentdb.com/api.php?amount=10&category=31&type=multiple
 
 export default Categories
