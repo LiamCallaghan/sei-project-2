d791d2b0ba4862d2c5fa0a3a6dec54d18f97d082
Anouska Ralph <anouskaoleary@gmail.com>
2020-09-17 19:00:34 +0100

    display answers/incorrect/correct success

--- src/component/game/Quiz.js
+++ src/component/game/Quiz.js
@@ -13,12 +13,42 @@ class Quiz extends React.Component {
     this.setState({
       questions: response.data
     })
-    console.log(response.data)
+    // console.log(response.data)
+    { this.questionLoop() }
+
   }
 
+  questionLoop = () => {
+    const questions = this.state.questions.results.map(result => (result.question))
+    return questions   
+  }
+  answerLoop = () => {
+    const answer = this.state.questions.results.map(result => (result.correct_answer))
+    return answer  
+  }
+  incorrectLoop = () => {
+    const incorrectAnswers = this.state.questions.results.map(result => (result.incorrect_answers))
+    const incorrectAnswer = incorrectAnswers.map(result => (result))
+    return incorrectAnswer  
+
+  }
+
+
   render() {
-    // if ( !this.state.questions ) return null
-    return <div>hello</div>
+    if ( !this.state.questions ) return null
+    return (
+      <section className="section">
+        <div className="container">
+          {this.questionLoop().map(question => (<h1 key={[question]}>{question}</h1>))}
+          {this.answerLoop().map(answer => (<h1 key={[answer]}>{answer}</h1>))}
+          {this.incorrectLoop().map(wrong => (<h1 key={[wrong]}>{wrong}</h1>))}
+        </div>
+
+      </section>
+
+    )
+
+
   }
 }
 
